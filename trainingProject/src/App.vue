767d3671ea321b37a66777f06c81bc7b47840c62
Diana Ishkildina <dianchic99@mail.ru>
2024-11-24 17:28:04 +0300

    Исправлены замечания

--- trainingProject/src/App.vue
+++ trainingProject/src/App.vue
@@ -1,3 +1,28 @@
+<template>
+<div class="wrap"> 
+    <div class="city-input">  
+        <h1> Погодное приложение</h1>
+        <input type="text" v-model="city" placeholder="Введите название города" @keydown.enter="getWeatherInfo">
+        <button :disabled="city ==''" @click="getWeatherInfo"> {{ buttonName }} </button>
+    </div>
+
+    <div class="city-name">
+        <template v-if="weather != null">
+            <img src="@/assets/map.svg" />
+            <h1> {{ showCityName }} </h1>
+        </template>
+        <p v-else> {{ error }}</p>
+    </div>   
+
+    <Temperature :temp="weather" :getWeatherInfo="getWeatherInfo" :getFTemp="getFTemp" :isButtonOneActive="isButtonOneActive" />
+
+    <Sun :weather="weather" />
+
+    <Condition :weather="weather"/>
+        
+</div>
+</template>
+
 <script>
 import axios from 'axios';
 import Temperature from './components/Temperature.vue';
@@ -9,7 +34,7 @@ export default {
     Temperature,
     Sun,
     Condition
-  },
+    },
     data() {
         return {
             city: "",
@@ -23,7 +48,7 @@ export default {
         }
     },
     methods: {
-        get_data() {
+        getWeatherInfo() {
             if(this.city.trim().length < 2) {
                 this.error = "Введите более одного символа"
                 this.weather = null;
@@ -36,14 +61,14 @@ export default {
             if (err.response && err.response.status === 404) {
                 this.error = "Такого города нет";
                 this.weather = null;
-            } else {
-                this.error = "Произошла ошибка. Попробуйте снова.";
-                this.weather = null;
-            }
+                return;
+            } 
+            this.error = "Произошла ошибĸа. Попробуйте снова.";
+            this.weather = null
             })
             this.isButtonOneActive = true
         },
-        getFtemp() {
+        getFTemp() {
             axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&lang=ru&appid=9bc72beca7fd1dafc25b81e5fb32b98e`)
             .then (res => (this.weather = res.data))
             this.isButtonOneActive = false
@@ -53,40 +78,16 @@ export default {
     computed: {
         showCityName() {
             return this.weather.name
+        },
+        buttonName() {
+            return this.city =='' ? 'Введите название' : 'Узнать погоду'
         }
     }
 }
 
 </script>
 
-<template>
-    <div class="wrap"> 
-        <div style="grid-area: header;">  
-        <h1> Погодное приложение</h1>
-        <input type="text" v-model="city" placeholder="Введите название города" @keydown.enter="get_data">
-        <button disabled v-if="city ==''"> Введите название</button>
-        <button v-else @click = "get_data"> Узнать погоду</button>
-        </div>
-
-        <div class="infoCity">
-            <template v-if="weather">
-                <img src = "@/assets/map.svg" />
-                <h1 style="margin: 0;"> {{ showCityName }}</h1>
-            </template>
-            <p v-else> {{ error }}</p>
-        </div>   
-
-        <Temperature :temp = "weather" :get_data = "get_data" :getFtemp = "getFtemp" :isButtonOneActive = "isButtonOneActive" />
-
-        <Sun :weather = "weather" />
-
-        <Condition :weather = "weather"/>
-        
-    </div>
-</template>
-
 <style>
-
 .wrap {
     display: grid;
     gap: 30px 30px;
@@ -137,13 +138,14 @@ export default {
 .wrap button:hover {
     transform: scale(1.1) translateY(-5px);
 }
-
-.infoCity {
+.city-name {
     display: flex;
     grid-area: city;  
     align-items: center; 
     justify-content: center;
     border-radius: 10px;
 }
-
+.city-input {
+    grid-area: header;
+}
 </style>
